7290aa2eee219ee516821ee6b91ad35bc5354c1b
sdodson <sc.dodson4@gmail.com>
2024-04-28 22:06:27 -0400

    Add experiences content collection

---
+++ src/content/config.ts
@@ -0,0 +1,31 @@
+import { z, defineCollection, reference } from "astro:content";
+
+const experienceCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    position: z.string(),
+    location: z.string(),
+    highlights: z.array(z.string()),
+    skills: z.array(reference("skill")),
+    logoUrl: z.string(),
+    startDate: z.string().transform((value) => new Date(value)),
+    endDate: z
+      .string()
+      .transform((value) => new Date(value))
+      .nullable(),
+  }),
+});
+
+const skillCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    logoUrl: z.string(),
+  }),
+});
+
+export const collections = {
+  experience: experienceCollection,
+  skill: skillCollection,
+};
